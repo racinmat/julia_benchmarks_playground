a435b7f0d8a838c73456296eb9b4731e7650a390
Matěj Račinský <matej.racinsky@avast.com>
2021-09-16 13:45:15 +0200

    raised versions of mill and jsongrinder, benchmarking zygote 0.6

--- run_bench.bat
+++ run_bench.bat
@@ -1,2 +1,4 @@
 "C:\Users\racinsky\AppData\Local\Programs\Julia 1.5.4\bin\julia.exe" mill.jl
 C:\Users\racinsky\AppData\Local\Programs\Julia-1.6.0\bin\julia.exe mill.jl
+C:\Users\racinsky\AppData\Local\Programs\Julia-1.6.1\bin\julia.exe mill.jl
+C:\Users\racinsky\AppData\Local\Programs\Julia-1.7.0-beta3\bin\julia.exe mill.jl
