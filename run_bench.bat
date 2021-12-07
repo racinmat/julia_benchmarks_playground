849378400e8f1f18f09644b94b94ce53d46138e0
Matěj Račinský <matej.racinsky@avast.com>
2021-12-07 08:47:09 +0100

    added some benchmark notes about mill

--- run_bench.bat
+++ run_bench.bat
@@ -3,4 +3,6 @@
 C:\Users\racinsky\AppData\Local\Programs\Julia-1.6.0\bin\julia.exe main.jl
 C:\Users\racinsky\AppData\Local\Programs\Julia-1.6.1\bin\julia.exe main.jl
 C:\Users\racinsky\AppData\Local\Programs\Julia-1.6.2\bin\julia.exe main.jl
+C:\Users\racinsky\AppData\Local\Programs\Julia-1.6.4\bin\julia.exe main.jl
 C:\Users\racinsky\AppData\Local\Programs\Julia-1.7.0-beta3\bin\julia.exe main.jl
+C:\Users\racinsky\AppData\Local\Programs\Julia-1.7.0\bin\julia.exe main.jl
